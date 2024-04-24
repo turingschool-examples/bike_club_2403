224df7b0fdea39b18ab78c04a35233e416210781
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-24 09:23:04 -0600

    Add Ride with total_distance and initialize methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,24 @@
+
 class Ride
+    attr_reader :name, 
+                :distance, 
+                :loop, 
+                :terrain
+
+    def initialize(params)
+        @name = params[:name]
+        @distance = params[:distance]
+        @loop = params[:loop]
+        @terrain = params[:terrain]
+    end
+
+    def total_distance
+        if @loop
+            actual_distance = distance
+        else 
+            actual_distance = distance * 2 
+        end
+        actual_distance
+    end
 
 end
