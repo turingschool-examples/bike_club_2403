e48d6387597e712a915d56f9f414f201a7db4781
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-24 09:42:35 -0600

    Added new instance variables to initialize method

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,11 +1,14 @@
 class Biker
     attr_reader :name, 
-                :max_distance
+                :max_distance,
+                :rides,
+                :acceptable_terrain
 
     def initialize(name, max_distance)
         @name = name
         @max_distance = max_distance
-        
+        @rides = {}
+        @acceptable_terrain = []
     end
 
     
