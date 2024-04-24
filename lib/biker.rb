418604f5041f1cd892499b2226f0a788b2df9bca
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-24 09:38:41 -0600

    Add initialize method in Biker

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,3 +1,12 @@
 class Biker
+    attr_reader :name, 
+                :max_distance
 
+    def initialize(name, max_distance)
+        @name = name
+        @max_distance = max_distance
+        
+    end
+
+    
 end
