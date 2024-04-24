9aea6098efd848d7d2435203cea39046e1883a67
Austin Kenny <austinkennywork@gmail.com>
2024-04-24 11:21:38 -0400

    Added test code for loops as well

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -25,5 +25,10 @@ describe Ride do
             ride1 = Ride.new({name: 'Walnut Creek Trail', distance: 10.7, loop: false, terrain: :hills})
             expect(ride1.total_distance).to eq(21.4)
         end
+
+        it 'returns the distance if it is a loop' do
+            ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+            expect(ride2.total_distance).to eq(14.9)
+        end
     end
 end
